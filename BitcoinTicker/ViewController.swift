47cf51fc611033a454280cc8e3d6e4ac125f7ae1
Monk <monk@industriallogic.com>
2018-12-27 15:04:54 -0800

    Have view controller conform to UI Picker protocol and updated read me

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -3,37 +3,32 @@
 //  BitcoinTicker
 //
 //  Created by Angela Yu on 23/01/2016.
+//  Edited by Monk Wellington on 27/12/2018
 //  Copyright © 2016 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
     var finalURL = ""
 
     //Pre-setup IBOutlets
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
-    
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
     }
 
-    
-    //TODO: Place your 3 UIPickerView delegate methods here
-    
-    
-    
 
-    
-    
-    
+    //TODO: Place your 3 UIPickerView delegate methods here
+
+
 //    
 //    //MARK: - Networking
 //    /***************************************************************/
@@ -76,10 +71,13 @@ class ViewController: UIViewController {
 //        
 //        updateUIWithWeatherData()
 //    }
-//    
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 0
+    }
 
-
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 0
+    }
 }
 
